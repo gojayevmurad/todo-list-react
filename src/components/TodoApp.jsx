2381d2cef57d9caf03bd4f8bac13e781aa2d3bd6
Murad Qocayev <muradqocayevi20@gmail.com>
2023-02-20 14:18:10 +0400

    filter added

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import TodoForm from "./TodosForm/TodoForm";
 import List from "./TodosList/List";
-import './style/styles.css'
+import "./style/styles.css";
 
 const TodoApp = () => {
-  const [todos, setTodos] = useState([{todoText:"asdf", isChecked: true},{todoText:"sadf", isChecked: false}]);
+  const [todos, setTodos] = useState([]);
   return (
     <div className="todo--app">
       <TodoForm todos={todos} setTodos={setTodos} />
--- src/components/TodosList/List.jsx
+++ src/components/TodosList/List.jsx
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from "react";
 
 const List = ({ todos, setTodos }) => {
-  const changeCheck = (index) => {
+  const [category, setCategory] = useState(null);
+  //! CHANGE CHECK
+  const changeCheck = (todo) => {
     let changedTodo = [...todos];
-    changedTodo[index].isChecked = !changedTodo[index].isChecked;
+    changedTodo.filter((el) => {
+      el == todo && (el.isChecked = !el.isChecked);
+    });
     setTodos(changedTodo);
   };
 
-  
-  
-  const filtered = todos.filter(element=>{
-    return 
-  })
+  //! FILTERED ARRAY
+  let filtered;
+  if (category == null) {
+    filtered = todos;
+  } else {
+    filtered = todos.filter((todo) => todo.isChecked == category);
+  }
 
- 
+  //! CLEAR COMPLETED TODOS
 
+  const clearCompleted = () => {
+    let completed = todos.filter((todo) => {
+      return todo.isChecked == false;
+    });
+
+    setTodos(completed);
+  };
+
+  //! COUNTER ACTIVE TODOS
   let counter = 0;
-
   for (let i = 0; i < todos.length; i++) {
     if (!todos[i].isChecked) {
       counter++;
@@ -25,27 +39,41 @@ const List = ({ todos, setTodos }) => {
 
   return (
     <div className="todo--app__list">
-      {todos.map((todo, index) => {
+      {filtered.map((todo, index) => {
         return (
           <div key={index} className="todo--app__list--todo">
-            <input className="todo--app__list--todo__checkbox"
-              onChange={() => changeCheck(index)}
+            <input
+              className="todo--app__list--todo__checkbox"
+              onChange={() => changeCheck(todo)}
               type="checkbox"
               checked={todo.isChecked}
             />
-            <span id={todo.isChecked? 'gray': ' '} className="todo--app__list--todo__text">{todo.todoText}</span>
+            <span
+              id={todo.isChecked ? "gray" : " "}
+              className="todo--app__list--todo__text"
+            >
+              {todo.todoText}
+            </span>
           </div>
         );
       })}
 
       <div className="actions">
-        <p><span>{counter}</span> items left</p>
+        <p>
+          <span>{counter}</span> items left
+        </p>
         <ul>
-          <li><button>All</button></li>
-          <li><button>Active</button></li>
-          <li><button>Completed</button></li>
+          <li>
+            <button onClick={() => setCategory(null)}>All</button>
+          </li>
+          <li>
+            <button onClick={() => setCategory(false)}>Active</button>
+          </li>
+          <li>
+            <button onClick={() => setCategory(true)}>Completed</button>
+          </li>
         </ul>
-        <button>Clear completed</button>
+        <button onClick={clearCompleted}>Clear completed</button>
       </div>
     </div>
   );
